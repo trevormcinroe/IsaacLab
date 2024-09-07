40e4776f83782dfa406e2851ded668e04f508423
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 17:09:36 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -297,7 +297,7 @@ class LiftEnv(DirectRLEnv):
     def _setup_scene(self):
         self.robot = Articulation(self.cfg.robot_cfg)
 
-        print(f'Heres the robot: {self.robot}')
+        print(f'Heres the robot: {self.robot.set_rigid_body_color}')
         qqq
 
         self.object = RigidObject(self.cfg.object_cfg)
