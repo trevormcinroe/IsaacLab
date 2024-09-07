86fd0d10cbf46cf7f3ef51517c873e26b59d1800
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 17:41:12 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -194,9 +194,10 @@ class LiftEnvCfg(DirectRLEnvCfg):
     # target = [0.3, 0, 0.5]
     # perception
     # object pos=[0.5, 0, 0.055]
+    # x [down-left, 0, up-right]
     tiled_camera: TiledCameraCfg = TiledCameraCfg(
         prim_path="/World/envs/env_.*/Camera",
-        offset=TiledCameraCfg.OffsetCfg(pos=(0.33, -0.3, 0.7), rot=(1,0,0,0), convention="world"),
+        offset=TiledCameraCfg.OffsetCfg(pos=(0.33, -0.35, 0.7), rot=(1,0,0,0), convention="world"),
         data_types=["rgb"],
         spawn=sim_utils.PinholeCameraCfg(
             focal_length=3.0, focus_distance=1.0, horizontal_aperture=20.955, clipping_range=(0.01, 200.0)
