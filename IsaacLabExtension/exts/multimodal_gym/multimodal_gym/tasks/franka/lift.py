b0727206ee3c02b58cc87bf3d530930d70ad2efc
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 09:44:38 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -206,7 +206,7 @@ class LiftEnvCfg(DirectRLEnvCfg):
 
     write_image_to_file = True
     frame_stack = 1
-    eye = [0, 1.5, 0.5]
+    eye = [0.5, 1.5, 0.5] # orig: [0, 1.5, 0.5]
     target = [0.3, 0, 0.5]
 
 
