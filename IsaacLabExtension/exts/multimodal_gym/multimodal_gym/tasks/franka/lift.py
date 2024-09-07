48f2ff7405b32ab9130afa84da9bf5c43e890005
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 13:17:54 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -418,7 +418,8 @@ class LiftEnv(DirectRLEnv):
             print(f'cam: {self._tiled_camera._view}\n')
             # self._tiled_camera.cfg.return_latest_camera_pose = True
             # self._tiled_camera.set_world_poses_from_view(eyes=eyes, targets=targets)
-            print(f'OUTPUT: {self._tiled_camera.set_world_poses_from_view(eyes=eyes, targets=targets)}')
+            self.scene.sensors["tiled_camera"].set_world_poses_from_view(eyes=eyes, targets=targets)
+
             print(f'cam: {self._tiled_camera._view}')
             self.init = True
         data_type = "rgb" if "rgb" in self.cfg.tiled_camera.data_types else "depth"
