56bf2854778e7018b1bffe8238d51b801a01d7f1
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 16:49:06 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -198,7 +198,7 @@ class LiftEnvCfg(DirectRLEnvCfg):
         offset=TiledCameraCfg.OffsetCfg(pos=(-2.0, 10.0, 20.0), rot=(1,0,0,0), convention="world"),
         data_types=["rgb"],
         spawn=sim_utils.PinholeCameraCfg(
-            focal_length=12.0, focus_distance=9000.0, horizontal_aperture=20.955, clipping_range=(0.01, 20.0)
+            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.01, 20.0)
         ),
         width=512,
         height=512,
