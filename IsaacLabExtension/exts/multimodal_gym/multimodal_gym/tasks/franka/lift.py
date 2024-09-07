98cf7110b111877596f085739d2d833cab8ddecf
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 13:32:10 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -417,14 +417,15 @@ class LiftEnv(DirectRLEnv):
             print(f'targets: {targets} // {targets.shape}')
             print(f'cam: {self._tiled_camera._view}\n')
             # self._tiled_camera.cfg.return_latest_camera_pose = True
-            # self._tiled_camera.set_world_poses_from_view(eyes=eyes, targets=targets)
-            self.scene.sensors["tiled_camera"].set_world_poses_from_view(eyes=eyes, targets=targets)
+            self._tiled_camera.set_world_poses_from_view(eyes=eyes, targets=targets)
+            self._tiled_camera._update_outdated_buffers()
+            # self.scene.sensors["tiled_camera"].set_world_poses_from_view(eyes=eyes, targets=targets)
 
             print(f'cam: {self._tiled_camera._view}')
             self.init = True
         data_type = "rgb" if "rgb" in self.cfg.tiled_camera.data_types else "depth"
-        # img_batch = self._tiled_camera.data.output[data_type].clone()
-        img_batch = self.scene.sensors["tiled_camera"].data.output[data_type].clone()
+        img_batch = self._tiled_camera.data.output[data_type].clone()
+        # img_batch = self.scene.sensors["tiled_camera"].data.output[data_type].clone()
         batch_size = img_batch.size()[0]
         flattened_images = img_batch.view(batch_size, -1)
 
