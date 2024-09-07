ee342064dd2f547de2e8f0eb519121ca8b6f1e40
trevor <t.mcinroe@ed.ac.uk>
2024-09-07 09:51:54 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -405,6 +405,7 @@ class LiftEnv(DirectRLEnv):
 
     def _get_images(self):
         if not self.init:
+            print(f'\n\nWITHIN _get_images(): {self.num_envs} // {self.cfg.eye}\n\n')
             eyes = torch.tensor(self.cfg.eye, dtype=torch.float, device=self.device).repeat((self.num_envs, 1)) + self.scene.env_origins
             targets = torch.tensor(self.cfg.target, dtype=torch.float, device=self.device).repeat((self.num_envs, 1)) + self.scene.env_origins
             self._tiled_camera.set_world_poses_from_view(eyes=eyes, targets=targets)
