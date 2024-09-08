a67a716bc897847c053a6f76a0bad0712ce9c5ca
trevor <t.mcinroe@ed.ac.uk>
2024-09-08 21:06:41 +0100

    camera adj

--- IsaacLabExtension/scripts/skrl/train.py
+++ IsaacLabExtension/scripts/skrl/train.py
@@ -322,20 +322,20 @@ def main(env_cfg, agent_cfg: dict):
 
     # .reshape(64, 84, 84, 9)
     # torch.Size([64, 84, 84, 9])
-    print(out.shape)
-    frames = [out.reshape(args_cli.num_envs, args_cli.hw, args_cli.hw, 3 * args_cli.frame_stack)]
-    for _ in range(50):
-        action = torch.tensor([env.action_space.sample() for _ in range(args_cli.num_envs)])
-        # print(f'action: {action}')
-        next_obs, reward, term, trunc, info = env.step(action)
-        # print(f'next: {next_obs.shape}')
-        # qqq
-        frames.append(next_obs.reshape(args_cli.num_envs, args_cli.hw, args_cli.hw, 3 * args_cli.frame_stack))
-
-    frames = torch.concat([x.unsqueeze(1) for x in frames], 1)[0].cpu()[:, :, :, :3].transpose(1, -1)
-    print(f'frames: {frames.shape}')
-    wandb.log({'video': wandb.Video((frames.numpy() * 255).astype(np.uint8), fps=30)})
-    qqq
+    # print(out.shape)
+    # frames = [out.reshape(args_cli.num_envs, args_cli.hw, args_cli.hw, 3 * args_cli.frame_stack)]
+    # for _ in range(50):
+    #     action = torch.tensor([env.action_space.sample() for _ in range(args_cli.num_envs)])
+    #     # print(f'action: {action}')
+    #     next_obs, reward, term, trunc, info = env.step(action)
+    #     # print(f'next: {next_obs.shape}')
+    #     # qqq
+    #     frames.append(next_obs.reshape(args_cli.num_envs, args_cli.hw, args_cli.hw, 3 * args_cli.frame_stack))
+    #
+    # frames = torch.concat([x.unsqueeze(1) for x in frames], 1)[0].cpu()[:, :, :, :3].transpose(1, -1)
+    # print(f'frames: {frames.shape}')
+    # wandb.log({'video': wandb.Video((frames.numpy() * 255).astype(np.uint8), fps=30)})
+    # qqq
 
     # Every call to .train() is 10k env steps
     for step in range(10_000_000 // (5_000 * args_cli.num_envs)):
