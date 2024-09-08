a667c425dd0083759dd9bf7aeb3c150b46689bd6
trevor <t.mcinroe@ed.ac.uk>
2024-09-08 19:37:13 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/models.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/models.py
@@ -527,7 +527,7 @@ class ImageEncoder(nn.Module):
         # obs = obs.view(batch_size, self.num_channels, self.img_dim, self.img_dim)
 
         # obs = obs.view(self.num_channels, batch_size, self.img_dim, self.img_dim)
-        obs = obs.reshape(64, 9, 84, 84)
+        obs = obs.reshape(64, 84, 84, 9).transpose(1, 2)
 
         from multimodal_gym.utils.image_utils import save_images_to_file
         #
@@ -535,7 +535,7 @@ class ImageEncoder(nn.Module):
         file_path = '/home/tmci/IsaacLab/IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.png'
         import numpy as np
         from PIL import Image
-        obs = np.array(obs[0, :3].transpose(0, -1).cpu() * 255).astype(np.uint8)
+        obs = np.array(obs[0, :, :, :3].cpu() * 255).astype(np.uint8)
         img = Image.fromarray(obs)
         img.save(file_path)
         # save_images_to_file(
