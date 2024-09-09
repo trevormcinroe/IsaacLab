bb02206e87d64c452adb6cc3cb70ea3152762cfe
trevor <t.mcinroe@ed.ac.uk>
2024-09-09 17:58:09 +0100

    camera adj

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/models.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/models.py
@@ -544,8 +544,7 @@ class ImageEncoder(nn.Module):
         # obs = obs.view(batch_size, self.img_dim, self.img_dim, self.num_channels).transpose(-1, 1)
         # obs = obs.reshape(64, 9, 84, 84)
 
-        from multimodal_gym.utils.image_utils import save_images_to_file
-        from torchvision.utils import save_image
+        from multimodal_gym.utils.image_utils import save_images_to_file, save_image
 
 
         save_image(obs, img_name='lift.png', nchw=True)
