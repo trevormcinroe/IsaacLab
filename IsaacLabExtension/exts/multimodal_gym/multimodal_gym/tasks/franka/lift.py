4364dfb942be7c88f47e4b85808e74194a08f7a4
trevor <t.mcinroe@ed.ac.uk>
2024-09-10 14:32:52 +0100

    w3rk

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/tasks/franka/lift.py
@@ -229,7 +229,7 @@ class LiftEnvCfg(DirectRLEnvCfg):
 
     write_image_to_file = True
     frame_stack = 1
-    eye = [1.0, 1.0, 1.0] # orig: [0, 1.5, 0.5]
+    eye = [1.0, 0.75, 0.75] # orig: [0, 1.5, 0.5]
     target = [0.0, 0.0, 0.5]  # orig: [0.3, 0.0, 0.5]
 
 class LiftEnv(DirectRLEnv):
