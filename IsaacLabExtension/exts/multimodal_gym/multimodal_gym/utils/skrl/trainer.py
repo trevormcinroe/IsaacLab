739346a3134f002ffa95cc39485236f09c7e96f7
trevor <t.mcinroe@ed.ac.uk>
2024-09-15 16:58:50 +0100

    adding extra logged info

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/trainer.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/trainer.py
@@ -574,7 +574,7 @@ class SequentialTrainer(Trainer):
 
                 mask_update = 1 - torch.logical_or(terminated, truncated).float()
                 mask *= mask_update
-                print(f'mask: {mask}\n=====')
+                print(f'mask: {mask.reshape(-1)}\n=====')
                 for k, v in infos['log'].items():
                     if k in returns:
                         returns[k] += v * mask
