7f6da25a39df152111c2230e27124e217267b028
trevor <t.mcinroe@ed.ac.uk>
2024-09-17 10:16:49 +0100

    adding in curriculum

--- IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/trainer.py
+++ IsaacLabExtension/exts/multimodal_gym/multimodal_gym/utils/skrl/trainer.py
@@ -574,6 +574,8 @@ class SequentialTrainer(Trainer):
 
                 # The below counter is used to trigger curriculum learning. We do not want eval steps
                 # to count towards this trigger point.
+                print(self.env)
+                qqq
                 self.env.common_step_counter -= 1
 
                 mask_update = 1 - torch.logical_or(terminated, truncated).float()
